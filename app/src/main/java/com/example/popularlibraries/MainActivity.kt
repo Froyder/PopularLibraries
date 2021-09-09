f92e58a44e57ff734ba11e5bec8dad8442e174e7
Froyder <homelik@mail.ru>
2021-09-09 08:30:16 +0300

    added proper functions for each button

--- app/src/main/java/com/example/popularlibraries/MainActivity.kt
+++ app/src/main/java/com/example/popularlibraries/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.example.popularlibraries
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.popularlibraries.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), MainView {
@@ -15,21 +14,22 @@ class MainActivity : AppCompatActivity(), MainView {
         vb = ActivityMainBinding.inflate(layoutInflater)
         setContentView(vb?.root)
 
-        val listener = View.OnClickListener {
-            presenter.counterClick(it.id)
-        }
+        vb?.btnCounter1?.setOnClickListener { presenter.buttonOneClicked() }
 
-        vb?.btnCounter1?.setOnClickListener(listener)
-        vb?.btnCounter2?.setOnClickListener(listener)
-        vb?.btnCounter3?.setOnClickListener(listener)
+        vb?.btnCounter2?.setOnClickListener { presenter.buttonTwoClicked() }
+
+        vb?.btnCounter3?.setOnClickListener { presenter.buttonThreeClicked() }
     }
 
-    //Подсказка к ПЗ: поделить на 3 отдельные функции и избавиться от index
-    override fun setButtonText(index: Int, text: String) {
-        when(index){
-            0 -> vb?.btnCounter1?.text = text
-            1 -> vb?.btnCounter2?.text = text
-            2 -> vb?.btnCounter3?.text = text
-        }
+    override fun setButtonOneText(text: String) {
+        vb?.btnCounter1?.text = text
+    }
+
+    override fun setButtonTwoText(text: String) {
+        vb?.btnCounter2?.text = text
+    }
+
+    override fun setButtonThreeText(text: String) {
+        vb?.btnCounter3?.text = text
     }
 }
